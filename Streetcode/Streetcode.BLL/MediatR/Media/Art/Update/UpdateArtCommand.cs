8eb9566c1bccce7abf8800cf8f3d3ccf76537d63
Andriy Kravets <kraandriy14904@gmail.com>
2025-05-24 22:36:52 +0300

    Implement admin gallery block

---
+++ Streetcode/Streetcode.BLL/MediatR/Media/Art/Update/UpdateArtCommand.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Media.Art;
+
+namespace Streetcode.BLL.MediatR.Media.Art.Update
+{
+    public record UpdateArtCommand(UpdateArtRequestDTO ArtUpdateRequest) : IRequest<Result<ArtDTO>>;
+}
