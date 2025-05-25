ff8fbf5a3bfcd18cdfe356f892132d6d7ba256f0
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-05-25 14:26:21 +0300

    feat/90: Add Position column to facts table and update types
    This commit introduces a new nullable integer column named `Position` to the `facts` table in the database schema. It also modifies the `StreetcodeType` column in the `streetcodes` table to have a maximum length of 21 characters, replacing the previous `nvarchar(max)` type. Similarly, the `CoordinateType` column in the `coordinates` table is updated to have a maximum length of 21 characters. These changes are part of a migration to ensure the database schema is optimized for data integrity and storage efficiency.

---
+++ Streetcode/Streetcode.DAL/Persistence/Migrations/20250525083440_Added a position to the Fact entity.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Streetcode.DAL.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedapositiontotheFactentity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "StreetcodeType",
+                schema: "streetcode",
+                table: "streetcodes",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Position",
+                schema: "streetcode",
+                table: "facts",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoordinateType",
+                schema: "add_content",
+                table: "coordinates",
+                type: "nvarchar(21)",
+                maxLength: 21,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Position",
+                schema: "streetcode",
+                table: "facts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "StreetcodeType",
+                schema: "streetcode",
+                table: "streetcodes",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(21)",
+                oldMaxLength: 21);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CoordinateType",
+                schema: "add_content",
+                table: "coordinates",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(21)",
+                oldMaxLength: 21);
+        }
+    }
+}
