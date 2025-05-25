4fff8fa12f1c648c655ec510bb760ff5f6084c25
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-05-25 14:31:44 +0300

    feat/90: Add reordering functionality to FactController
    Introduced a new HTTP PATCH endpoint at "{streetcodeId:int}/Reorder" in the FactController. This allows clients to send a collection of FactReorderDTOs to reorder facts. Added a using directive for Streetcode.BLL.MediatR.Streetcode.Fact.Reorder to support this functionality.

--- Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/FactController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Streetcode/TextContent/FactController.cs
@@ -4,6 +4,7 @@ using Streetcode.BLL.MediatR.Streetcode.Fact.Create;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetAll;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetById;
 using Streetcode.BLL.MediatR.Streetcode.Fact.GetByStreetcodeId;
+using Streetcode.BLL.MediatR.Streetcode.Fact.Reorder;
 
 namespace Streetcode.WebApi.Controllers.Streetcode.TextContent;
 
@@ -31,5 +32,11 @@ public class FactController : BaseApiController
     public async Task<IActionResult> Create([FromBody] FactUpdateCreateDTO fact)
     {
         return HandleResult(await Mediator.Send(new CreateFactCommand(fact)));
+    }
+
+    [HttpPatch("{streetcodeId:int}/Reorder")]
+    public async Task<IActionResult> Reorder([FromBody] IEnumerable<FactReorderDTO> factReorderDtos, [FromRoute] int streetcodeId)
+    {
+        return HandleResult(await Mediator.Send(new ReorderFactsCommand(factReorderDtos, streetcodeId)));
     }
 }
