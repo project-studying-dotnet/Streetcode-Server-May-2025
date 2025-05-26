99a9d153f06b2bf30ac2854a38f18cf3622e22a0
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-05-26 18:58:09 +0300

    refactor/84: Add PartnerConfiguration for EF Core entity setup
    This commit introduces the `PartnerConfiguration` class that implements `IEntityTypeConfiguration<Partner>`. It configures the one-to-many relationship between `Partner` and `PartnerSourceLinks`, ensuring cascading deletes. The `IsKeyPartner` property is set to have a default value of "false". Necessary namespaces for Entity Framework Core are also included.

---
+++ Streetcode/Streetcode.DAL/Configurations/Partners/PartnerConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Streetcode.DAL.Entities.Partners;
+
+namespace Streetcode.DAL.Configurations.Partners;
+
+public class PartnerConfiguration : IEntityTypeConfiguration<Partner>
+{
+    public void Configure(EntityTypeBuilder<Partner> builder)
+    {
+        builder
+            .HasMany(d => d.PartnerSourceLinks)
+            .WithOne(p => p.Partner)
+            .HasForeignKey(d => d.PartnerId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder
+            .Property(p => p.IsKeyPartner)
+            .HasDefaultValue("false");
+    }
+}
