bae763be27dcb9ab4bcd6cc3541d19141291bcbd
Andriy Kravets <kraandriy14904@gmail.com>
2025-05-29 15:09:43 +0300

    Create folder Analytics in DTO, create dto class and record and create StreetcodeContentDTO

---
+++ Streetcode/Streetcode.BLL/DTO/Analytics/StatisticRecordCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.BLL.DTO.Analytics;
+
+public record StatisticRecordCreateDTO
+{
+    int? QrId { get; init; }
+    int Count { get; init; }
+    public string Address { get; init; }
+    public int StreetcodeId { get; init; }
+    public int StreetcodeCoordinateId { get; init; }
+}
---
+++ Streetcode/Streetcode.BLL/DTO/Analytics/StatisticRecordDTO.cs
@@ -0,0 +1,21 @@
+﻿using Streetcode.BLL.DTO.AdditionalContent.Coordinates.Types;
+using Streetcode.BLL.DTO.Streetcode;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streetcode.BLL.DTO.Analytics;
+
+public class StatisticRecordDTO
+{
+    public int Id { get; set; }
+    public int QrId { get; set; }
+    public int Count { get; set; }
+    public string Address { get; set; }
+    public int StreetcodeId { get; set; }
+    public StreetcodeContentDTO? Streetcode { get; set; }
+    public int StreetcodeCoordinateId { get; set; }
+    public StreetcodeCoordinateDTO StreetcodeCoordinate { get; set; }
+}
