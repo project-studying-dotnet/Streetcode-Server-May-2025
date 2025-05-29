3b6d09c198e96563f5f51c0b83b004b8857e6f00
Andriy Kravets <kraandriy14904@gmail.com>
2025-05-29 15:15:51 +0300

    Create folder Analytics in Mapping and create mapper StatisticRecordProfile

---
+++ Streetcode/Streetcode.BLL/Mapping/Analytics/StatisticRecordProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Streetcode.BLL.DTO.Analytics;
+using Streetcode.DAL.Entities.Analytics;
+
+namespace Streetcode.BLL.Mapping.Analytics;
+
+public class StatisticRecordProfile : Profile
+{
+    public StatisticRecordProfile()
+    {
+        CreateMap<StatisticRecord, StatisticRecordDTO>().ReverseMap();
+    }
+}
