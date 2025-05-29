b859ae533bcf0406c1d1f4c4b7d9fa46adda63b1
Ilbko <ilbkopro60000@gmail.com>
2025-05-29 08:59:20 +0300

    updaterelatedtermhandler and test cleanup

--- Streetcode/Streetcode.BLL/MediatR/Streetcode/RelatedTerm/Update/UpdateRelatedTermHandler.cs
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/RelatedTerm/Update/UpdateRelatedTermHandler.cs
@@ -1,14 +1,10 @@
-﻿using AutoMapper;
-using FluentResults;
+﻿using FluentResults;
 using MediatR;
-using Streetcode.DAL.Repositories.Interfaces.Base;
 
 namespace Streetcode.BLL.MediatR.Streetcode.RelatedTerm.Update;
 
 public class UpdateRelatedTermHandler : IRequestHandler<UpdateRelatedTermCommand, Result<Unit>>
 {
-    public UpdateRelatedTermHandler(IMapper mapper, IRepositoryWrapper repository) { }
-
     public Task<Result<Unit>> Handle(UpdateRelatedTermCommand request, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
