83243559335389005122ef6347d9723e31d550f0
Andriy Kravets <kraandriy14904@gmail.com>
2025-05-29 15:33:07 +0300

    Add folder Analytics in MediatR and create two files Command and Handler

---
+++ Streetcode/Streetcode.BLL/MediatR/Analytics/CreateStatisticRecordCommand.cs
@@ -0,0 +1,7 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Analytics;
+
+namespace Streetcode.BLL.MediatR.Analytics;
+
+public record CreateStatisticRecordCommand(StatisticRecordCreateDTO StatisticRecordCreateDTO) : IRequest<Result<StatisticRecordDTO>>;
---
+++ Streetcode/Streetcode.BLL/MediatR/Analytics/CreateStatisticRecordHandler.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using FluentResults;
+using MediatR;
+using Streetcode.BLL.DTO.Analytics;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.DAL.Entities.Analytics;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Analytics;
+
+public class CreateStatisticRecordHandler : IRequestHandler<CreateStatisticRecordCommand, Result<StatisticRecordDTO>>
+{
+    private readonly IMediator _mediator;
+    private readonly IRepositoryWrapper _repositoryWrapper;
+    private readonly ILoggerService _logger;
+    private readonly IMapper _mapper;
+
+    public CreateStatisticRecordHandler(
+        IMediator mediator,
+        IRepositoryWrapper repositoryWrapper,
+        ILoggerService logger,
+        IMapper mapper)
+    {
+        _mediator = mediator;
+        _repositoryWrapper = repositoryWrapper;
+        _logger = logger;
+        _mapper = mapper;
+    }
+
+    public async Task<Result<StatisticRecordDTO>> Handle(CreateStatisticRecordCommand request, CancellationToken cancellationToken)
+    {
+        var statisticRecordCreateDTO = request.StatisticRecordCreateDTO;
+
+        if (statisticRecordCreateDTO == null)
+        {
+            _logger.LogWarning("CreateStatisticRecordCommand: StatisticRecordCreateDTO is null.");
+            return Result.Fail(new Error("Statistic record data is required."));
+        }
+
+        var statisticRecord = _mapper.Map<StatisticRecord>(statisticRecordCreateDTO);
+        await _repositoryWrapper.StatisticRecordRepository.CreateAsync(statisticRecord);
+        await _repositoryWrapper.SaveChangesAsync();
+        _logger.LogInformation("CreateStatisticRecordCommand: Statistic record created successfully.");
+        var statisticRecordDTO = _mapper.Map<StatisticRecordDTO>(statisticRecord);
+        return Result.Ok(statisticRecordDTO);
+    }
+}
