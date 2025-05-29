424342191a31b2966f8275e97e3f53011688bbcc
Andriy Kravets <kraandriy14904@gmail.com>
2025-05-29 15:41:41 +0300

    Create endpoint Create to StatisticRecordController

---
+++ Streetcode/Streetcode.WebApi/Controllers/Analytics/StatisticRecordController.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Streetcode.BLL.DTO.Analytics;
+using Streetcode.BLL.MediatR.Analytics;
+
+namespace Streetcode.WebApi.Controllers.Analytics;
+
+public class StatisticRecordController : BaseApiController
+{
+    [Authorize(Roles = "Administrator")]
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] StatisticRecordCreateDTO statisticRecordCreateDTO)
+    {
+        return HandleResult(await Mediator.Send(new CreateStatisticRecordCommand(statisticRecordCreateDTO)));
+    }
+}
