daaa26c3499681bf1a8ca64ccd09d4823fceb0ca
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-06-05 23:20:39 +0300

    feat/214: Add JwtSettings class for JWT configuration
    Introduces a new `JwtSettings` class in the
    `UserService.WebApi.Configurations` namespace. This class
    contains properties for `Key`, `Issuer`, `Audience`,
    `AccessTokenExpirationMinutes`, and `RefreshTokenExpirationDays`
    to facilitate JWT (JSON Web Token) configuration in the application.

---
+++ Streetcode/UserService.WebApi/Configurations/JwtSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace UserService.WebApi.Configurations;
+
+public class JwtSettings
+{
+    public string Key { get; set; } = null!;
+    public string Issuer { get; set; } = null!;
+    public string Audience { get; set; } = null!;
+    public int AccessTokenExpirationMinutes { get; set; }
+    public int RefreshTokenExpirationDays { get; set; }
+}
