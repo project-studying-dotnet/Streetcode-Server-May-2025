7ce3105ba3141b03c538dc7be2b8daf4b1c9f2af
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-06-06 12:06:59 +0300

    feat/214: Add refresh token repository interface and implementation
    Introduces the `IRefreshTokenRepository` interface for managing refresh tokens, including methods for adding, retrieving, updating, and saving changes asynchronously. Implements this interface in the `RefreshTokenRepository` class, which uses `UserServiceDbContext` and Entity Framework Core for database operations related to refresh tokens.

---
+++ Streetcode/UserService.WebApi/Data/Repositories/Realisations/RefreshTokenRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserService.WebApi.Data.Repositories.Interfaces;
+using UserService.WebApi.Entities.Auth;
+
+namespace UserService.WebApi.Data.Repositories.Realisations;
+
+public class RefreshTokenRepository : IRefreshTokenRepository
+{
+    private readonly UserServiceDbContext _dbContext;
+
+    public RefreshTokenRepository(UserServiceDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task AddAsync(RefreshToken refreshToken, CancellationToken cancellationToken)
+    {
+        await _dbContext.RefreshTokens.AddAsync(refreshToken, cancellationToken);
+    }
+
+    public async Task<RefreshToken?> GetByTokenAsync(string token, CancellationToken cancellationToken)
+    {
+        return await _dbContext.RefreshTokens
+            .Include(rt => rt.User)
+            .SingleOrDefaultAsync(rt => rt.Token == token, cancellationToken);
+    }
+
+    public Task UpdateAsync(RefreshToken refreshToken, CancellationToken cancellationToken)
+    {
+        _dbContext.RefreshTokens.Update(refreshToken);
+
+        return Task.CompletedTask;
+    }
+
+    public async Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        return await _dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
