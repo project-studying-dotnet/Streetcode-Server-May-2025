334377141346195389041434ff48732772e1ea16
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-06-08 18:02:48 +0300

    feat/224: Update token service return types and imports
    Changed return types in `ITokenService` and `TokenService` from `Result<TokenResultDTO>` to `Result<TokenResponseDTO>` for `GenerateTokensAsync` and `RefreshAccessTokenAsync` methods. Updated import statement to use `UserService.WebApi.DTO.Auth.Responses` to reflect the new response DTO structure.

--- Streetcode/UserService.WebApi/Services/Interfaces/ITokenService.cs
+++ Streetcode/UserService.WebApi/Services/Interfaces/ITokenService.cs
@@ -1,14 +1,14 @@
 ﻿using FluentResults;
-using UserService.WebApi.DTO.Auth;
+using UserService.WebApi.DTO.Auth.Responses;
 using UserService.WebApi.Entities.Users;
 
 namespace UserService.WebApi.Services.Interfaces;
 
 public interface ITokenService
 {
-    Task<Result<TokenResultDTO>> GenerateTokensAsync(User user, CancellationToken cancellationToken);
+    Task<Result<TokenResponseDTO>> GenerateTokensAsync(User user, CancellationToken cancellationToken);
 
-    Task<Result<TokenResultDTO>> RefreshAccessTokenAsync(string refreshToken, CancellationToken cancellationToken);
+    Task<Result<TokenResponseDTO>> RefreshAccessTokenAsync(string refreshToken, CancellationToken cancellationToken);
 
     Task<Result<bool>> RevokeRefreshTokenAsync(string refreshToken, CancellationToken cancellationToken);
 }
