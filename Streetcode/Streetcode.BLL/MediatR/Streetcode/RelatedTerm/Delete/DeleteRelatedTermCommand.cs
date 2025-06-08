42dbe515721fdda3612fa436a01f08dd7ea63a8a
Viktoriia Yashan <yashanviktoriya@gmail.com>
2025-06-08 11:30:03 +0300

    updated handler, command, controller and related tests for unlinking related terms

--- Streetcode/Streetcode.BLL/MediatR/Streetcode/RelatedTerm/Delete/DeleteRelatedTermCommand.cs
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/RelatedTerm/Delete/DeleteRelatedTermCommand.cs
@@ -4,4 +4,4 @@ using Streetcode.BLL.DTO.Streetcode.TextContent;
 
 namespace Streetcode.BLL.MediatR.Streetcode.RelatedTerm.Delete;
 
-public record DeleteRelatedTermCommand(string word) : IRequest<Result<RelatedTermDTO>>;
+public record DeleteRelatedTermCommand(string word, int termId) : IRequest<Result<RelatedTermDTO>>;
