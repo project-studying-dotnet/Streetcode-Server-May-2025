c2e89e9d94a795d506025f7064bdba72a8dc9b13
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-06-09 15:07:46 +0300

    feat/225: Add DeleteCommentHandlerI for comment deletion logic
    This commit introduces the `DeleteCommentHandlerI` class, which implements the `IRequestHandler<DeleteCommentCommand, Result<Unit>>` interface. The class is responsible for handling comment deletions, including retrieving comments by ID, logging errors for non-existent comments, and managing the deletion process. It ensures that appropriate success or failure results are returned based on the outcome of the deletion operation.

---
+++ Streetcode/Streetcode.BLL/MediatR/Streetcode/Comment/Delete/DeleteCommentHandler.cs
@@ -0,0 +1,45 @@
+﻿using FluentResults;
+using MediatR;
+using Streetcode.BLL.Interfaces.Logging;
+using Streetcode.DAL.Repositories.Interfaces.Base;
+
+namespace Streetcode.BLL.MediatR.Streetcode.Comment.Delete;
+
+public class DeleteCommentHandlerI : IRequestHandler<DeleteCommentCommand, Result<Unit>>
+{
+    private readonly IRepositoryWrapper _repositoryWrapper;
+    private readonly ILoggerService _logger;
+
+    public DeleteCommentHandlerI(IRepositoryWrapper repositoryWrapper, ILoggerService logger)
+    {
+        _repositoryWrapper = repositoryWrapper;
+        _logger = logger;
+    }
+
+    public async Task<Result<Unit>> Handle(DeleteCommentCommand request, CancellationToken cancellationToken)
+    {
+        var commentId = request.CommentId;
+
+        var comment = await _repositoryWrapper.CommentRepository.GetFirstOrDefaultAsync(c => c.Id == commentId);
+        if (comment is null)
+        {
+            var errorMsg = $"Comment deletion failed: no comment found with ID '{commentId}'.";
+            _logger.LogError(request, errorMsg);
+
+            return Result.Fail(errorMsg);
+        }
+
+        _repositoryWrapper.CommentRepository.Delete(comment);
+
+        var changesSaved = await _repositoryWrapper.SaveChangesAsync() > 0;
+        if (!changesSaved)
+        {
+            var errorMsg = $"Comment deletion failed: unable to persist deletion for comment ID '{commentId}'.";
+            _logger.LogError(request, errorMsg);
+
+            return Result.Fail(new Error(errorMsg));
+        }
+
+        return Result.Ok(Unit.Value);
+    }
+}
