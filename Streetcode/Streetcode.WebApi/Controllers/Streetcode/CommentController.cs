e7bcfa2d5a3836fcfd7dc5a534bc03412c778eeb
Nikita Kobylynskyi <kobilinskiyn@gmail.com>
2025-06-09 15:08:03 +0300

    feat/225: Add Delete action to CommentController
    This commit introduces a new `Delete` method in the `CommentController` class, enabling the deletion of comments by their ID through an HTTP DELETE request. The method utilizes the `DeleteCommentCommand` for processing the deletion. Additionally, the necessary using directive for `DeleteCommentCommand` has been added to the top of the file.

--- Streetcode/Streetcode.WebApi/Controllers/Streetcode/CommentController.cs
+++ Streetcode/Streetcode.WebApi/Controllers/Streetcode/CommentController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Streetcode.BLL.MediatR.Streetcode.Comment.Delete;
 using Streetcode.BLL.MediatR.Streetcode.Comment.GetByStreetcodeId;
 
 namespace Streetcode.WebApi.Controllers.Streetcode;
@@ -9,6 +10,14 @@ public class CommentController : BaseApiController
     public async Task<IActionResult> GetByStreetcodeId([FromRoute] int streetcodeId)
     {
         var result = await Mediator.Send(new GetCommentsByStreetcodeIdQuery(streetcodeId));
+        return HandleResult(result);
+    }
+
+    [HttpDelete("{commentId:int}")]
+    public async Task<IActionResult> Delete([FromRoute] int commentId)
+    {
+        var result = await Mediator.Send(new DeleteCommentCommand(commentId));
+
         return HandleResult(result);
     }
 } 
