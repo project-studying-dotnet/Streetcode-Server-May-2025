2053fd743d7527dfb6309aaf738b5b9401a9bc11
KobMiles <kobilinskiyn@gmail.com>
2025-06-15 19:07:05 +0300

    feat/268: Add ServiceBusExtensions for Azure Service Bus integration
    This commit introduces a new static class `ServiceBusExtensions` in the `UserService.WebApi.Extensions` namespace. It includes the method `AddAzureServiceBusIntegration`, which extends `IServiceCollection` to configure `ServiceBusSettings` from the application configuration. Additionally, it registers a singleton instance of `ServiceBusClient` using the connection string from the settings, facilitating easier setup and management of Azure Service Bus in the application.

---
+++ Streetcode/UserService.WebApi/Extensions/ServiceBusExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Options;
+using UserService.WebApi.Configurations;
+
+namespace UserService.WebApi.Extensions;
+
+public static class ServiceBusExtensions
+{
+    public static IServiceCollection AddAzureServiceBusIntegration(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.Configure<ServiceBusSettings>(
+            configuration.GetSection("AzureServiceBus")
+        );
+
+        services.AddSingleton(sp =>
+        {
+            var settings = sp
+                .GetRequiredService<IOptions<ServiceBusSettings>>()
+                .Value;
+
+            return new ServiceBusClient(settings.ConnectionString);
+        });
+
+        return services;
+    }
+}
