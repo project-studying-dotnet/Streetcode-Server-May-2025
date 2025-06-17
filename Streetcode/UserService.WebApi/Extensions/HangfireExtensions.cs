1195b23cbce1f0f006d452fe5e25c5a668c4ced5
KobMiles <kobilinskiyn@gmail.com>
2025-06-17 18:43:40 +0300

    feat/249: Add Hangfire extensions for SQL Server and job scheduling
    Introduces a new static class `HangfireExtensions` in the
    `UserService.WebApi.Extensions` namespace. This class includes
    two extension methods: `AddHangfireServerWithSqlStorage` for
    configuring Hangfire with SQL Server storage and
    `UseHangfireDashboardAndScheduler` for setting up the
    Hangfire dashboard and scheduling recurring jobs based on
    configuration settings.

---
+++ Streetcode/UserService.WebApi/Extensions/HangfireExtensions.cs
@@ -0,0 +1,63 @@
+﻿using Hangfire;
+using Hangfire.SqlServer;
+using UserService.WebApi.Services.Interfaces;
+
+namespace UserService.WebApi.Extensions;
+
+public static class HangfireExtensions
+{
+    public static IServiceCollection AddHangfireServerWithSqlStorage(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        string? hangfireConnectionString = configuration
+                                              .GetConnectionString("HangfireConnection");
+
+        services.AddHangfire(cfg => cfg
+            .UseSqlServerStorage(
+                hangfireConnectionString,
+                new SqlServerStorageOptions
+                {
+                    PrepareSchemaIfNecessary = true,
+                }));
+
+        services.AddHangfireServer();
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseHangfireDashboardAndScheduler(
+    this IApplicationBuilder app,
+    IConfiguration configuration)
+    {
+        app.UseHangfireDashboard();
+
+        var recurringJobsSection = configuration.GetSection("Hangfire:RecurringJobs");
+
+        foreach (var jobSection in recurringJobsSection.GetChildren())
+        {
+            var jobKey = jobSection.Key;
+            var jobId = jobSection.GetValue<string>("JobId");
+            var cronSchedule = jobSection.GetValue<string>("Cron");
+
+            if (string.IsNullOrWhiteSpace(jobId) || string.IsNullOrWhiteSpace(cronSchedule))
+            {
+                continue;
+            }
+
+            switch (jobKey)
+            {
+                case "RevokeExpiredTokens":
+                    RecurringJob.AddOrUpdate<ITokenService>(
+                        recurringJobId: jobId,
+                        methodCall: svc => svc
+                            .RevokeExpiredRefreshTokensAsync(CancellationToken.None),
+                        cronExpression: cronSchedule
+                    ); 
+                    break;
+            }
+        }
+
+        return app;
+    }
+}
